a0c4e9db12fab1ae9cd146f1af66af423604076c
Jacob Sayono <jrsayono@gmail.com>
2023-11-02 01:07:46 -0700

    bug fix

--- launch/bruinbot_sim.launch.py
+++ launch/bruinbot_sim.launch.py
@@ -15,8 +15,6 @@ def generate_launch_description():
 
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
     package_name='a-ros'
 
     bruinbot = IncludeLaunchDescription(
